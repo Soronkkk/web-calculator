6853815f5352e5a9bac1419a1d0b4c8dd8e19594
Soronk <soronk99@gmail.com>
2019-07-01 19:47:29 +0400

    Add gitignore file

--- src/main/java/app/servlets/Login.java
+++ src/main/java/app/servlets/Login.java
@@ -5,20 +5,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class Login extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/login.jsp");
-        requestDispatcher.forward(req, resp);
 
         String uname = req.getParameter("uname");
         String pass = req.getParameter("pass");
 
         if (uname.equals("login") && pass.equals("password")) {
+            HttpSession session = req.getSession(true);
+            session.setAttribute("currentSessionUser","user");
             resp.sendRedirect("views/welcome.jsp");
         }
+
     }
 }

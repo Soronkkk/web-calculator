02abf6d92de03cff0a7a153036c921152a28af77
Soronk <soronk99@gmail.com>
2019-07-04 11:59:23 +0400

    add logout

--- src/main/java/app/servlets/Login.java
+++ src/main/java/app/servlets/Login.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class Login extends HttpServlet {
@@ -20,8 +21,12 @@ public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String uname = req.getParameter("uname");
         String pass = req.getParameter("pass");
-        if(uname.equals("user") && pass.equals("pass")){
+        if (uname.equals("user") && pass.equals("pass")) {
+            HttpSession session = req.getSession();
+            session.setAttribute("uname", uname);
+            System.out.println("logined");
             req.getRequestDispatcher("index.html").forward(req, resp);
+
         } else {
             req.getRequestDispatcher("/views/login.jsp").forward(req, resp);
         }
